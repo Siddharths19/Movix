17cb6368595c96975920eed927929464062b6237
Siddharth Singh <siddharth.singh190902@gmail.com>
2023-07-10 02:23:43 +0530

    fixed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./Home.scss";
 
-import HeroBanner from "./heroBanner/heroBanner";
+import HeroBanner from "./heroBanner/HeroBanner";
 import Trending from "./trending/Trending";
 import Popular from "./popular/Popular";
 import TopRated from "./topRated/TopRated";
